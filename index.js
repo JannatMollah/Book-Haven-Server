1c3617a5205a3a673802ac942f67a5bb91801453
JannatMollah <jannatmollah@gmail.com>
2025-11-13 21:06:12 +0600

    test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ClientEncryption, ObjectId } = require('mongodb');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceKey.json");
+require("dotenv").config()
 const app = express()
 const port = 3000
 
@@ -12,7 +15,12 @@ app.get('/', (req, res) => {
 })
 
 
-const uri = "mongodb+srv://BookHaven:C635xrEtUSNNkNFW@cluster0.wbnbgws.mongodb.net/?appName=Cluster0";
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wbnbgws.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,79 +31,107 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization
+  if (!authorization) {
+    res.status(401).send({
+      message: "token not found"
+    })
+  }
+  const token = authorization.split(' ')[1]
+
+
+
+  try {
+    await admin.auth().verifyIdToken(token)
+    next()
+  } catch (error) {
+    res.status(401).send({
+      message: "unauthorized access"
+    })
+  }
+
+  next()
+
+}
+
+
+
+
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db('BookHaven')
     const bookCollection = db.collection('Books')
 
     app.get('/books', async (req, res) => {
-        const result = await bookCollection.find().toArray()
+      const result = await bookCollection.find().toArray()
 
-        res.send(result)
+      res.send(result)
     })
 
     app.post('/books', async (req, res) => {
       const data = req.body
-      console.log(data);
-      
-        const result = await bookCollection.insertOne(data)
-        res.send({
-          success: true,
-          result
-        })
+
+      const result = await bookCollection.insertOne(data)
+      res.send({
+        success: true,
+        result
+      })
     })
 
-    app.get('/books/:id', async (req, res) => {
-      const {id} = req.params
+    app.get('/books/:id', verifyToken, async (req, res) => {
+      const { id } = req.params
       const objectid = new ObjectId(id)
-        const result = await bookCollection.findOne({_id: objectid})
-        res.send({
-          success: true,
-          result
-        })
+      const result = await bookCollection.findOne({ _id: objectid })
+      res.send({
+        success: true,
+        result
+      })
     })
 
     app.put('/books/:id', async (req, res) => {
-      const {id} = req.params
+      const { id } = req.params
       const data = req.body
       const objectid = new ObjectId(id)
-      const filter = {_id: objectid}
+      const filter = { _id: objectid }
       const update = {
         $set: data
       }
-        const result = await bookCollection.updateOne(filter, update)
+      const result = await bookCollection.updateOne(filter, update)
 
-        res.send({
-          success: true,
-          result
-        })
+      res.send({
+        success: true,
+        result
+      })
     })
 
     app.delete('/books/:id', async (req, res) => {
-      const {id} = req.params
+      const { id } = req.params
       const objectid = new ObjectId(id)
 
-        const result = await bookCollection.deleteOne({_id: objectid})
+      const result = await bookCollection.deleteOne({ _id: objectid })
 
-        res.send({
-          success: true,
-          result
-        })
+      res.send({
+        success: true,
+        result
+      })
     })
 
     app.get('/latest', async (req, res) => {
-        const result = await bookCollection.find().limit(8).toArray()
+      const result = await bookCollection.find().limit(8).toArray()
 
-        res.send(result)
+      res.send(result)
     })
 
 
 
 
 
-    await client.db("admin").command({ ping: 1 });
+
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
